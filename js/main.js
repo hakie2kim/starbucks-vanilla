82fc714e1292c24da4bde087a5d37afaf16a024d
unknown <hakie2kim@gmail.com>
2024-01-03 17:42:01 +0900

    feat(css/main.css|index.html|js/main.js): add promotion image slides pt.1

--- js/main.js
+++ js/main.js
@@ -57,3 +57,22 @@ new Swiper(".notice-line .swiper-container", {
   autoplay: true,
   loop: true,
 });
+
+new Swiper(".promotion .swiper-container", {
+  // direction: "horizontal", // 기본값
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수 (기본값 1)
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  loop: true, // 반복 재생
+  autoplay: {
+    delay: 5000, // 기본값 5초
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination", // 페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-prev",
+    nextEl: ".promotion .swiper-next",
+  },
+});
